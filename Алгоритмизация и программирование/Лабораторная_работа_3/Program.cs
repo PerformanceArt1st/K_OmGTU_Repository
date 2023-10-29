cf0f13564ff07cc4c3dadf1aad7986e25dd564ce
Daniil_Cybulskij <84662143+PerformanceArt1st@users.noreply.github.com>
2023-10-30 02:16:59 +0600

    Add files via upload

---
+++ Алгоритмизация и программирование/Лабораторная_работа_3/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double y = 0, a = 2.2, b = 0.3, i; //y - результат вычислений; i - переменные, подаваемые на вход; a, b - заданные переменные;
+
+            Console.WriteLine("Программа для вычисления значения функции y в зависимости от интервала, в котором лежит значение переменной i");
+            Console.WriteLine("    |  a*i^4 + b*i               при i < 10");
+            Console.WriteLine("y = |  tg(i + 0.5)               при i = 10");
+            Console.WriteLine("    |  e^(2*i) + √(a^2 + i^2)    при i > 10");
+
+            Console.WriteLine("Введите i принадлежащее интервалу [7,12] : ");
+            i = Convert.ToDouble(Console.ReadLine());
+
+            if (i < 10 && i >= 7)
+            {
+                y = a * Math.Pow(i, 4) + b * i;
+                Console.WriteLine($"y({i}) = {y}");
+            }
+            else if (i == 10)
+            {
+                y = Math.Tan(i + 0.5);
+                Console.WriteLine($"y({i}) = {y}");
+            }
+            else if (i > 10 && i <= 12)
+            {
+                y = Math.Pow(Math.E, 2 * i) + Math.Sqrt(Math.Pow(a, 2) + Math.Pow(i, 2));
+                Console.WriteLine($"y({i}) = {y}");
+            }
+            else
+            {
+                Console.WriteLine($"Вводимый аргумент i не принадлежит интервалу [7,12]");
+            }
+        }
+    }
+}
---
+++ Алгоритмизация и программирование/Лабораторная_работа_4.2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_4_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = 0, b = 2 * Math.PI, n, f1, f2; //a, b, n - переменные, подаваемые на вход; f1, f2 - переменные, хранящие значения функций;
+
+            Console.WriteLine("Программа для вычисления значений функций (5-3 cosx) и (√(1+sin^2x)) в n равномерно распределенных в диапазоне а<=x<=b точках");
+            Console.WriteLine("Введите n: ");
+            n = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine();
+            Console.WriteLine($"|{"x",-30}|{"F1(х)",-30}|{"F2(х)",-30}|");
+            Console.WriteLine(new string('—', 94));
+
+
+            for (double x = a; x < b; x += (b - a) / n)
+            {
+                f1 = 5 - 3 * Math.Cos(x);
+                f2 = Math.Sqrt(1 + Math.Pow(Math.Sin(x), 2));
+                Console.Write($"|{x, - 30}|");
+                Console.Write($"{f1, - 30}|");
+                Console.Write($"{f2, - 30}|");
+                Console.WriteLine();
+            }
+        }
+    }
+}
