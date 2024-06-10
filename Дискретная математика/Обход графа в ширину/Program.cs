6163169ac71db981c74435d888a1c77f8f6e616a
Daniil_Cybulskij <84662143+PerformanceArt1st@users.noreply.github.com>
2024-06-11 00:06:05 +0600

    Add files via upload

---
+++ Дискретная математика/Обход графа в ширину/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int c = 1;
+        int v = 8;
+        List<int>[] adj = new List<int>[v];
+        for (int i = 0; i < v; i++)
+        {
+            adj[i] = new List<int>();
+        }
+
+        adj[0].Add(1);
+        adj[0].Add(2);
+        adj[0].Add(4);
+        adj[1].Add(2);
+        adj[1].Add(4);
+        adj[2].Add(4);
+        adj[3].Add(5);
+        adj[3].Add(6);
+        adj[3].Add(7);
+        adj[5].Add(7);
+        adj[6].Add(7);
+
+        bool[] visited = new bool[v];
+
+        for (int i = 0; i < v; i++)
+        {
+
+            if (!visited[i])
+            {
+                Console.WriteLine("Вершины компоненты связности {0}:", c);
+                Queue<int> queue = new Queue<int>();
+                visited[i] = true;
+                queue.Enqueue(i);
+                while (queue.Count > 0)
+                {
+                    int w = queue.Dequeue();
+                    Console.Write(w+1 + " ");
+
+                    foreach (int n in adj[w])
+                    {
+                        if (!visited[n])
+                        {
+                            visited[n] = true;
+                            queue.Enqueue(n);
+                        }
+                    }
+                }
+                c++;
+                Console.WriteLine();
+            }
+        }
+    }
+}
