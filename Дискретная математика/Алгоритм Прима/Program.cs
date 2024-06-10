0e2e00ac434da4a07721272045c5641eb1328333
Daniil_Cybulskij <84662143+PerformanceArt1st@users.noreply.github.com>
2024-06-10 23:31:58 +0600

    Add files via upload

---
+++ Дискретная математика/Алгоритм Прима/Program.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    class Graph
+    {
+        public int Start { get; set; }
+        public int End { get; set; }
+        public int Weight { get; set; }
+    }
+
+    static void Main(string[] args)
+    {
+        int sum = 0;
+        int v = 5;
+        List<Graph> edges = new List<Graph>
+        {
+            new Graph { Start = 0, End = 3, Weight = 20 },
+            new Graph { Start = 0, End = 4, Weight = 10 },
+            new Graph { Start = 1, End = 2, Weight = 5 },
+            new Graph { Start = 1, End = 3, Weight = 2 },
+            new Graph { Start = 2, End = 3, Weight = 4 },
+            new Graph { Start = 2, End = 4, Weight = 3 },
+            new Graph { Start = 3, End = 4, Weight = 6 }
+        };
+
+        bool[] m = new bool[v];
+        int[] k = new int[v];
+        int[] res = new int[v];
+
+        for (int i = 0; i < v; i++)
+        {
+            k[i] = int.MaxValue;
+            m[i] = false;
+        }
+
+        k[0] = 0;
+        res[0] = -1;
+
+        for (int count = 0; count < v - 1; count++)
+        {
+            int u = -1;
+            int min = int.MaxValue;
+
+            for (int i = 0; i < v; i++)
+            {
+                if (!m[i] && k[i] < min)
+                {
+                    min = k[i];
+                    u = i;
+                }
+            }
+
+            m[u] = true;
+
+            for (int i = 0; i < edges.Count; i++)
+            {
+                int start = edges[i].Start;
+                int end = edges[i].End;
+                int weight = edges[i].Weight;
+                if ((start == u || end == u) && !m[end] && weight < k[end])
+                {
+                    k[end] = weight;
+                    res[end] = u;
+                }
+
+                if ((start == u || end == u) && !m[start] && weight < k[start])
+                {
+                    k[start] = weight;
+                    res[start] = u;
+                }
+            }
+        }
+        
+        Console.WriteLine($" Ребро ");
+        for (int i = 1; i < v; i++)
+        {
+            Console.WriteLine($"{i+1} ——— {res[i]+1}");
+            sum += k[i];
+        }
+        Console.WriteLine("Минимальная остова графа: " + sum);
+    }
+}
---
+++ Дискретная математика/Алгоритм Флойда/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int V = 4;
+
+        int[,] graph = {
+            { 0, 3, int.MaxValue, 5 },
+            { 2, 0, int.MaxValue, 4 },
+            { int.MaxValue, 1, 0, int.MaxValue },
+            { int.MaxValue, int.MaxValue, 2, 0 }
+        };
+
+        int[,] dist = new int[V, V];
+
+        for (int i = 0; i < V; i++)
+        {
+            for (int j = 0; j < V; j++)
+            {
+                dist[i, j] = graph[i, j];
+            }
+        }
+
+        for (int k = 0; k < V; k++)
+        {
+            for (int i = 0; i < V; i++)
+            {
+                for (int j = 0; j < V; j++)
+                {
+                    if (dist[i, k] != int.MaxValue && dist[k, j] != int.MaxValue && dist[i, k] + dist[k, j] < dist[i, j])
+                    {
+                        dist[i, j] = dist[i, k] + dist[k, j];
+                    }
+                }
+            }
+        }
+
+        Console.WriteLine("Кратчайшие расстояния между всеми парами вершин: ");
+        Console.WriteLine();
+
+        for (int i = 0; i < V; i++)
+        {
+            Console.Write("|");
+            for (int j = 0; j < V; j++)
+            {
+                if (dist[i, j] == int.MaxValue)
+                    Console.Write("int.MaxValue ");
+                else
+                    Console.Write(dist[i, j] + "\t");
+            }
+            Console.Write("|\n");
+        }
+    }
+}
